9f1a35ccdb3e6368913244efce47075a4f93a89c
Sthesia <seirios48@yahoo.co.jp>
2021-01-03 21:44:43 +0900

    add MRTK and Create HatenaBlock

---
+++ AnimeFantasyWorldDemo/Assets/SuperMarioBos_/Scripts/HatenaBlock.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class HatenaBlock : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audio;
+
+    [SerializeField]
+    UnityEvent BlockActiveEvents;
+
+
+    [SerializeField]
+    int BlockLife;
+
+    [SerializeField]
+    UnityEvent EndBlockEvents;
+    [SerializeField,Tooltip("")]
+    UnityEvent DeactiveBlockEvents;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnActiveHatenaBlockEvent()
+    {
+        if (BlockLife>0)
+        {
+            BlockActiveEvents.Invoke();
+
+            BlockLife -= 1;
+            if (BlockLife == 0)
+            {
+                EndBlockLifeEvent();
+            }
+        }
+        else
+        {
+            DeactiveBlockEvents.Invoke();
+        }
+    }
+
+    private void EndBlockLifeEvent()
+    {
+        EndBlockEvents.Invoke();    
+    }
+}
